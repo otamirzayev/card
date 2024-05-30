99253be6bbb25626d2e0d84a24b36cb60f2e51e0
mirzayev otbek <otabek.@gmail.com>
2024-05-30 15:25:10 +0300

    card

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,14 @@
+// style
+import './Footer.css'
+
+function Footer() {
+  return (
+   <div className='footer'>
+    <div footer-container>
+      <a href="https://www.youtube.com/watch?v=TBrufotgELQ&list=PLNS3PujVHR-awWJYJ74hFbgt8cHeTCC_b&index=35">About card</a>
+    </div>
+   </div>
+  )
+}
+
+export default Footer
